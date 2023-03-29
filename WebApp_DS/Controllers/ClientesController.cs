0bddd7a456857c3a4abd9db89144c75dc06ec19e
oliThai <oliveirath27@gmail.com>
2023-03-29 08:21:36 -0300

    Criado lista e controller clientes

---
+++ WebApp_DS/Controllers/ClientesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp_DS.Controllers
+{
+    public class ClientesController : Controller
+    {
+        public IActionResult Lista()
+        {
+            return View();
+        }
+    }
+}
