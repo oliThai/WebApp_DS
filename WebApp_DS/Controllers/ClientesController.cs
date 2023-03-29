f4d08ea675efb60f40610ffdb96a008b04364ccb
oliThai <oliveirath27@gmail.com>
2023-03-29 11:06:24 -0300

    Mensagem de erro, tela cadastro de cliente

--- WebApp_DS/Controllers/ClientesController.cs
+++ WebApp_DS/Controllers/ClientesController.cs
@@ -8,5 +8,30 @@ namespace WebApp_DS.Controllers
         {
             return View();
         }
+        public IActionResult Cadastro() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Cadastro(string Nome, string Telefone) 
+        {
+            if(string.IsNullOrEmpty(Nome))
+            {
+                TempData["Erro"] = "O campo nome não pode estar em branco";
+            }
+            if(string.IsNullOrEmpty(Telefone))
+            {
+                TempData["Erro"] = "O campo telefone não pode estar em branco";
+            }
+            return View();
+        }
+        public IActionResult Editar(int id) 
+        {
+            return View();
+        }
+        public IActionResult Exclui(int id)
+        {
+            return View();
+        }
     }
 }
