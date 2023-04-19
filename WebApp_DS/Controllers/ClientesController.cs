42b3fbb2cbecf23dfd740aa1445d21bed1d9b764
oliThai <oliveirath27@gmail.com>
2023-04-19 11:08:58 -0300

    Botões cadastrar novo cliente.  editar e excluir

--- WebApp_DS/Controllers/ClientesController.cs
+++ WebApp_DS/Controllers/ClientesController.cs
@@ -1,17 +1,47 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApp_DS.Models;
 
 namespace WebApp_DS.Controllers
 {
     public class ClientesController : Controller
     {
+        public static List<ClienteViewModel> lista = new List<ClienteViewModel>();
         public IActionResult Lista()
         {
-            return View();
+
+            //for (var i = 0; i < 16; i++)
+            //{
+            //    ClienteViewModel novo = new ClienteViewModel();
+            //    novo.Nome = "Cliente" + i;
+            //    novo.Id = 1 + i;
+            //    novo.Telefone = "16997892986";
+            //    Lista.Add(novo);
+            //}
+           
+            return View(lista);
+
         }
         public IActionResult Cadastro() 
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult SalvarDados(ClienteViewModel model)
+        {
+            if (model.Id > 0) {
+                int indice = lista.FindIndex(a => a.Id == model.Id);
+                lista[indice] = model;
+            }
+            else
+            {
+                Random rand = new Random();
+                model.Id = rand.Next(1,999);
+                lista.Add(model);
+            }
+            
+            return RedirectToAction("Lista");
+        }
+
         [HttpPost]
         public IActionResult Cadastro(string Nome, string Telefone) 
         {
@@ -27,11 +57,26 @@ namespace WebApp_DS.Controllers
         }
         public IActionResult Editar(int id) 
         {
+            ClienteViewModel cliente = lista.Find(a => a.Id == id);
+            if(cliente != null)
+            {
+                return View(cliente);
+            }
+            else
+            {
+                return RedirectToAction("Lista");
+            }
             return View();
         }
-        public IActionResult Exclui(int id)
+
+        public IActionResult Excluir(int id)
         {
-            return View();
+            ClienteViewModel cliente = lista.Find(a => a.Id == id);
+            if(cliente != null) 
+            {
+                lista.Remove(cliente);
+            }
+            return RedirectToAction("Lista");
         }
     }
 }
---
+++ WebApp_DS/Models/ClienteViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApp_DS.Models
+{
+    public class ClienteViewModel
+    {
+        public long Id { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; } 
+    }
+}
