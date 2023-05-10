6e3c8742e24d707f0a41d70c65400787e78ce0e7
oliThai <oliveirath27@gmail.com>
2023-05-10 10:49:24 -0300

    Botão editar e excluir categorias

--- WebApp_DS/Controllers/CategoriaController.cs
+++ WebApp_DS/Controllers/CategoriaController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebApp_DS.Entidades;
+using WebApp_DS.Models;
 
 namespace WebApp_DS.Controllers
 {
@@ -22,13 +23,48 @@ namespace WebApp_DS.Controllers
 
         [HttpPost]
 
-        public IActionResult SalvarDados(Categoria dados) {
+        public IActionResult SalvarDados(CategoriaViewModel dados) {
 
-            db.CATEGORIAS.Add(dados);
-            db.SaveChanges();
+            Categoria entidade = new Categoria();
+            entidade.NOME = dados.Nome;
+            entidade.ID = dados.Id;
+            entidade.ATIVO = dados.Ativo;
+
+            if(entidade.ID > 0) { 
+                db.CATEGORIAS.Update(entidade);
+                db.SaveChanges();
+            }
+            else
+            {
+                db.CATEGORIAS.Add(entidade);
+                db.SaveChanges();
+            }
+            
             return RedirectToAction("Lista"); 
         }
 
+        public ActionResult Excluir(int id) {
+            Categoria item = db.CATEGORIAS.Find(id);
+            if(item != null) {
+                db.CATEGORIAS.Remove(item); 
+                db.SaveChanges(); 
+            }
+
+            return RedirectToAction("Lista");
+        }
+
+        public IActionResult Editar(int id)
+        {
+            Categoria item = db.CATEGORIAS.Find(id);
+            if(item != null)
+            {
+                return View(item);
+            }
+            else
+            {
+                return RedirectToAction("Lista");
+            }
+        }
 
     }
 }
---
+++ WebApp_DS/Models/CategoriaViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApp_DS.Models
+{
+    public class CategoriaViewModel
+    {
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public bool Ativo { get; set; }
+    }
+}
